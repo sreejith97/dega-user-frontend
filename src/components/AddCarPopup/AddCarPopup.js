38227d6017d5d768e5d683b9846c0a841e33d6ca
Phinahas Philip <phinahasphilip2000@gmail.com>
2022-01-06 10:17:05 +0530

    User can add there vehicle name

--- src/components/AddCarPopup/AddCarPopup.js
+++ src/components/AddCarPopup/AddCarPopup.js
@@ -1,68 +1,81 @@
 import React, { useEffect, useState } from "react";
 import "./AddCarPopup.css";
-import axios from '../../axios';
-import {useSelector} from 'react-redux'
+import axios from "../../axios";
+import { useSelector } from "react-redux";
 
 function AddCarPopup({ closeAddPopup }) {
-
-  const user = useSelector((state)=>state.loggedInReducers.user);
-  const [vehicles, setVehicles] = useState([]);
+  const user = useSelector((state) => state.loggedInReducers.user);
+  const [vehicles, setVehicles] = useState();
   const [loading, setLoading] = useState(false);
+  const [selectedVehicle, setSelectedVehicle]= useState(null);
+  const [celaningTime , setCleaningTime] = useState(null);
+  const [registrationNumber, setRegistrationNumber] = useState();
+  const [parkingLot, setParkingLot] = useState();
   useEffect(() => {
-   
-    axios.get('/user/get-all-vehicles',{headers:{"auth-token":(user.token)}}).then((response)=>{
-      console.log(response.data);
-      if(response.data == "Invalid token"){
-        window.location.href ="/users/login"
-      }else{
+    axios
+      .get("/user/get-all-vehicles", { headers: { "auth-token": user.token } })
+      .then((response) => {
+        //console.log(response.data);
+        if (response.data == "Invalid token") {
+          window.location.href = "/users/login";
+        } else {
           setVehicles(response.data);
-      }
-    })
-   
-  }, [])
+        }
+      });
+  }, []);
+  const formSubmit = (e)=>{
+    e.preventDefault();
+    if(celaningTime == null){
+      window.alert("Please select cleaning time")
+      //console.log("hii");
+    }else if(selectedVehicle == null){
+      window.alert("Please select your vehicle")
+    }else{
+      axios.post('/user/register-user-vehicle',{"ownerId":user._id,"vehicleId":selectedVehicle,"parkingLot":parkingLot,"registrationNumber":registrationNumber,"cleaningTime":celaningTime},{"headers":{"auth-token":(user.token)}}).then((response)=>{
+        console.log(response.data);
+        if(response.data.error){
+          window.alert(response.data.error)
+        }else{
+          window.alert(response.data.success);
+          closeAddPopup(false);
+
+        }
+      })
+    }
+    //console.log(selectedVehicle, celaningTime, registrationNumber, parkingLot);
+
+  }
   return (
     <div>
-      <div onClick={() => closeAddPopup(false)} className="popup-background">
+      <div  className="popup-background">
         <div className="popup-container">
           <div className="flex justify-center mb-5">
             <div>
               <h1 className="uppercase font-extrabold text-2xl">Add New Car</h1>
+              {vehicles == null ? (<><h4>LOADING</h4></>):null}
             </div>
           </div>
-          <form action="">
+          <form action="" onSubmit={formSubmit}>
             <div className="input-container mb-5">
-              <div className="lable">Company Name</div>
+              <div className="lable">Vehicle Name</div>
               <div>
-                <input
-                  className=" p-3 focus:border-blue-500 rounded border-2 outline-none "
-                  type="text"
-                />
+                <select required name="cars" id="cars" onChange={(e)=>{setSelectedVehicle(e.target.value);}}>
+                  <option selected disabled>select</option>
+                  {vehicles != null ? (<>{vehicles.map(obj => {
+                    return (
+                        <option value={obj._id}>{obj.vehicleName}</option>
+                    )
+                  })}</>):(null)}
+                  
+                </select>
               </div>
             </div>
-            <div className="input-container mb-5">
-              <div className="lable">Model:</div>{" "}
-              <div>
-                <input
-                  className=" p-3 focus:border-blue-500 rounded border-2 outline-none "
-                  type="text"
-                />
-              </div>
-            </div>
-
-            <div className="input-container mb-5">
-              <div className="lable">Color:</div>{" "}
-              <div>
-                <input
-                  className=" p-3 focus:border-blue-500 rounded border-2 outline-none "
-                  type="text"
-                />
-              </div>
-            </div>
-
             <div className="input-container mb-5">
               <div className="lable">Reg Number:</div>{" "}
               <div>
                 <input
+                  required
+                  onChange={(e)=>{setRegistrationNumber(e.target.value)}}
                   className=" p-3 focus:border-blue-500 rounded border-2 outline-none "
                   type="text"
                 />
@@ -70,21 +83,24 @@ function AddCarPopup({ closeAddPopup }) {
             </div>
 
             <div className="input-container mb-5">
-              <div className="lable">Parking Slot:</div>{" "}
+              <div className="lable">Parking Lot:</div>{" "}
               <div>
                 <input
+                required
+                onChange={(e)=>{setParkingLot(e.target.value)}}
                   className=" p-3 focus:border-blue-500 rounded border-2 outline-none "
                   type="text"
                 />
               </div>
             </div>
             <div className="input-container mb-5">
-              <div className="lable">Year:</div>{" "}
+              <div className="lable">Cleaning Time:</div>{" "}
               <div>
-                <input
-                  className=" p-3 focus:border-blue-500 rounded border-2 outline-none "
-                  type="text"
-                />
+                <select required onChange={(e)=>setCleaningTime(e.target.value)}>
+                  <option selected disabled>select</option>
+                  <option  value="1">06:00 am - 08:00 am</option>
+                  <option value="2">04:00 pm - 06:00 pm</option>
+                </select>
               </div>
             </div>
             <div className="flex justify-between">
